5aaff145c51ebc71d752f629d21bc614db3b12dc
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-09-30 20:10:56 -0300

    mudando arquivos

--- Frases_motivacionais/recomendador_de_frases.py
+++ Frases_motivacionais/recomendador_de_frases.py
@@ -1,21 +1,15 @@
 #criaçao de um programa que recomenda frases motivacionais para o usuario usar em seu dia a dia
 #usar o streamlit para criar a interface
 
-#import streamlit as st
+import streamlit as st
 import pandas as pd
 
 #busca frase motivacional
 #importa dataframe online
-df = pd.read_csv('')
+df = pd.read_csv()
 
-#cria uma função para recomendar frases_motivacionais
-def recomenda_frase():
-    return df.sample()
+#ler o dataframe
+df
 
-if __name__ == '__main__':
-    #st.title('Recomendador de frases motivacionais')
-    #st.write('Clique no botão abaixo para receber uma frase motivacional')
-    #if st.button('Recomenda uma frase'):
-        #st.write(recomenda_frase())
-    teste = recomenda_frase()
-    print(recomenda_frase())
+
+
