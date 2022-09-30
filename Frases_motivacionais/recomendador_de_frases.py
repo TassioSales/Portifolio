95099cfeb5a4c1769729c7c70a049493a7a39d16
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-09-30 20:19:20 -0300

    Update recomendador_de_frases.py

--- Frases_motivacionais/recomendador_de_frases.py
+++ Frases_motivacionais/recomendador_de_frases.py
@@ -6,10 +6,24 @@ import pandas as pd
 
 #busca frase motivacional
 #importa dataframe online
-df = pd.read_csv()
+df = pd.read_csv('frases_motivacionaiss.csv', encoding='utf-8', sep=';')
 
-#ler o dataframe
-df
+#cria uma função para buscar a frases_motivacionaiss
+def busca_frase():
+    #cria um botão para buscar a frase
+    if st.button('Buscar frase motivacional'):
+        #cria uma variavel para receber a frase
+        frase = df['Frase_motivacionais'].sample(1).values[0]
+        #mostra a frase
+        st.write(frase)
+
+#mostra o titulo
+st.title('Recomendador de frases motivacionais')
+#mostra a frase
+st.write('Clique no botão abaixo para receber uma frase motivacional')
+#chama a função
+busca_frase()
+
 
 
 
