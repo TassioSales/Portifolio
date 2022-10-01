2d6ba09d9e596937f18e3bd86bab09ee587989a3
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-01 01:19:51 -0300

    Update recomendador_de_frases.py

--- Frases_motivacionais/recomendador_de_frases.py
+++ Frases_motivacionais/recomendador_de_frases.py
@@ -10,19 +10,21 @@ import random
 #importa dataframe online
 df_motivacional = pd.read_csv('https://raw.githubusercontent.com/TassioSales/Portifolio/main/Frases_motivacionais/frases_motivacionais.csv', sep=';', encoding='utf-8')
 df_demotivacional = pd.read_csv('https://raw.githubusercontent.com/TassioSales/Portifolio/main/Frases_motivacionais/frases_desmotivacionais.csv', sep=';', encoding='utf-8')
-df_cantadas = 
+df_cantadas = pd.read_csv('https://raw.githubusercontent.com/TassioSales/Portifolio/main/Frases_motivacionais/cantadas.csv', sep=';', encoding='utf-8')
 
 
-#cria uma lista com as frases
+#Criando listas (frases motivacionais, desmotivacionais e cantadas)
 frases_motivacionais = df_motivacional['Frases_Motivacionais'].tolist()
 fraser_desmotivacionais = df_demotivacional['Frase_desmotivacionais'].tolist()
+cantadas = df_cantadas['Cantadas'].tolist()
 
 
 lista_emoji = [':smile:', ':grinning:',':grin:', ':joy:', ':smiley:', ':laughing:', ':satisfied:', ':innocent:', ':stuck_out_tongue_winking_eye:', ':wink:', ':blush:', ':slight_smile:', ':upside_down_face:', ':relaxed:', ':yum:', ':relieved:', ':heart_eyes:', ':kissing_heart:', ':kissing:', ':kissing_smiling_eyes:', ':kissing_closed_eyes:', ':stuck_out_tongue_closed_eyes:', ':stuck_out_tongue:', ':money_mouth_face:', ':nerd_face:', ':sunglasses:', ':hugging_face:', ':smirk:', ':no_mouth:', ':neutral_face:', ':expressionless:', ':unamused:', ':rolling_eyes:', ':thinking:', ':flushed:', ':disappointed:', ':worried:', ':angry:', ':rage:', ':pensive:', ':confused:', ':slight_frown:', ':frowning2:', ':persevere:', ':confounded:', ':tired_face:', ':weary:', ':triumph:', ':open_mouth:', ':scream:', ':fearful:', ':cold_sweat:', ':hushed:', ':frowning:', ':anguished:', ':cry:', ':disappointed_relieved:', ':sleepy:', ':sweat:', ':sob:', ':dizzy_face:', ':astonished:', ':zipper_mouth_face:', ':mask:', ':thermometer_face:', ':head_bandage:', ':sleeping:', ':zzz:', ':poop:', ':smiling_imp:', ':imp:', ':japanese_ogre:', ':japanese_goblin:', ':skull:', ':ghost:', ':alien:', ':robot:', ':smiley_cat:', ':smile_cat:', ':joy_cat:', ':heart_eyes_cat:', ':smirk_cat:', ':kissing_cat:', ':scream_cat:', ':crying_cat_face:', ':pouting_cat:', ':raised_hands:', ':clap:', ':wave:', ':thumbsup:', ':thumbsdown:', ':punch:', ':fist:', ':v:', ':ok_hand:', ':raised_hand:', ':open_hands:', ':muscle:', ':pray:', ':point_up:', ':point_up_2:', ':point_down:', ':point_left:', ':point_right:', ':middle_finger:', ':hand_splayed:', ':metal:', ':vulcan_salute:', ':writing_hand:', ':nail_care:', ':lips:', ':tongue:', ':ear:', ':nose:', ':eye:', ':eyes:', ':bust_in_silhouette']
 
-#frase motivacional aleatoria
+##randomizando a lista
 frase_moti = random.choice(frases_motivacionais)
 frase_desmoti = random.choice(fraser_desmotivacionais)
+cantadas = random.choice(cantadas)
 
 #cria a interface
 st.title('Recomendador de frases')
@@ -49,7 +51,7 @@ st.write('')
 st.write('')
 #cria um botao para gerar uma cantada
 if st.button('Gerar Cantada'):
-    st.write('Você é como o sol, brilha todos os dias')
+    st.write(cantadas)
     st.write(random.choice(lista_emoji))
 
 #Obrigado por usar o programa
