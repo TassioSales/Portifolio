f779d30020beb6904a120831bd1de7c2c02566fc
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-02 17:31:04 -0300

    Update recomendador_de_frases.py

--- Frases_motivacionais/recomendador_de_frases.py
+++ Frases_motivacionais/recomendador_de_frases.py
@@ -11,7 +11,7 @@ import random
 df_motivacional = pd.read_csv('https://raw.githubusercontent.com/TassioSales/Portifolio/main/Frases_motivacionais/motivacionais.csv', sep=';', encoding='utf-8')
 df_demotivacional = pd.read_csv('https://raw.githubusercontent.com/TassioSales/Portifolio/main/Frases_motivacionais/frases_desmotivacionais.csv', sep=';', encoding='utf-8')
 df_cantadas = pd.read_csv('https://raw.githubusercontent.com/TassioSales/Portifolio/main/Frases_motivacionais/cantadas.csv', sep=';', encoding='utf-8')
-df_piadas_
+df_piadas_ruins = pd.read_csv('https://raw.githubusercontent.com/TassioSales/Portifolio/main/Frases_motivacionais/piadas.csv', sep=';', encoding='utf-8')
 
 
 #Criando listas (frases motivacionais, desmotivacionais e cantadas)
