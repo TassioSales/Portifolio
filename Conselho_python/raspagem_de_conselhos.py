e3dfe3c913aae9603da99792c14813ee692d8b7e
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-04 00:04:46 -0300

    Create raspagem_de_conselhos.py

---
+++ Conselho_python/raspagem_de_conselhos.py
@@ -0,0 +1,35 @@
+#criar webscraping para raspagem de dados de conselhos do dia a dia e colocar em um arquivo csv
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+
+link = 'https://br.pinterest.com/eunicealvesmelo/conselhos-importantes-para-o-dia-a-dia/'
+
+#funçao para fazer a requisição
+def requisicao(link):
+    requisita = requests.get(link)
+    return requisita
+
+#funçao para pegar o link das imagens
+def pegar_link_imagem(requisita):
+    soup = BeautifulSoup(requisita.text, 'html.parser')
+    imagens = soup.find_all('img')
+    return imagens
+
+#funçao para mostra o link das imagens
+def mostrar_link_imagem(imagens):
+    for imagem in imagens:
+        print(imagem['src'])
+
+
+#funçao para mostra uma imagem aleatoria
+def mostrar_imagem_aleatoria(imagens):
+    import random
+    imagem = random.choice(imagens)
+    print(imagem['src'])
+
+if __name__ == '__main__':
+    requisita = requisicao(link)
+    imagens = pegar_link_imagem(requisita)
+    mostrar_imagem_aleatoria(imagens)
+
