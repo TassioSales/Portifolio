931a26242197d74c0c791a856c7b05b4c89277e5
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-05 16:55:04 -0300

    Update app.py

--- Gerador_de_Conselho/app.py
+++ Gerador_de_Conselho/app.py
@@ -1,54 +1,65 @@
-#criar arquivo package.txt
-#pip3 freeze > package.txt
-#imports
+# criar arquivo package.txt
+# pip3 freeze > package.txt
+# imports
 
-#import biblioteca tradutor google
+# import biblioteca tradutor google
 import googletrans
-#import Streamlit
+# import Streamlit
 import streamlit as st
-#importar biblioteca para fazer requisições
+# importar biblioteca para fazer requisições
 import requests
+import traceback
 
-#criar funçao quer dar conselho aleatorio usando advise slip
+
+# criar funçao quer dar conselho aleatorio usando advise slip
 def get_advice():
     """
     Função que retorna um conselho aleatório
     return: str advice para o usuário
     """
-    #criar variavel que vai receber a resposta da api
+    # criar variavel que vai receber a resposta da api
     response = requests.get("https://api.adviceslip.com/advice")
-    #criar variavel que vai receber o json da resposta
+    # criar variavel que vai receber o json da resposta
     json_data = response.json()
-    #criar variavel que vai receber o conselho
+    # criar variavel que vai receber o conselho
     advice = json_data["slip"]["advice"]
-    #retornar o conselho
+    # retornar o conselho
     return advice
 
 
-#criar funçao que vai traduzir o conselho somente para portugues
-def traduzir_conselho():
-    
-
-
-
+# criar funçao que vai traduzir o conselho somente para portugues
+def traduzir_conselho(advice):
+    """
+    Função que traduz o conselho para português
+    return: str translated_advice para o usuário
+    """
+    # criar variavel que vai receber o tradutor
+    translator = googletrans.Translator()
+    # criar variavel que vai receber o conselho traduzido
+    translated_advice = translator.translate(advice, dest="pt")
+    # retornar o conselho traduzido
+    return translated_advice
 def main():
-    """
-    Função principal do programa
-    """
-    #criar variavel que vai receber o titulo do programa
-    st.title("Conselho do dia")
-    #criar variavel que vai receber o subtitulo do programa
-    st.subheader("Clique no botão para receber um conselho")
-    #criar variavel que vai receber o botão
-    button = st.button("Receber conselho")
-    #criar condicional que vai verificar se o botão foi clicado
-    if button:
-        #criar variavel que vai receber o conselho
-        advice = get_advice()
-        #criar variavel que vai receber o conselho traduzido
-        translated_advice = translate_advice(advice)
-        #criar variavel que vai receber o conselho
+    try:
+        # criar titulo
+        st.title("Conselho do dia")
+        # criar subtitulo
+        st.subheader("Clique no botão para receber um conselho")
+        # criar botão
+        button = st.button("Clique aqui")
+        # criar condicional para o botão
+        if button:
+            # criar variavel que vai receber o conselho
+            advice = get_advice()
+            # criar variavel que vai receber o conselho traduzido
+            translated_advice = traduzir_conselho(advice)
+            # criar titulo para o conselho
+            st.title("Conselho")
+            # criar subtitulo para o conselho
+            st.subheader(translated_advice.text)
+    except Exception:
+        st.write(traceback.format_exc())
+
 
 if __name__ == '__main__':
     main()
- 
