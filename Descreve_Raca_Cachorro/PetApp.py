85b88a81c79cf364d89f9da7355c1d582cc26118
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-05 18:18:28 -0300

    Update PetApp.py

--- Descreve_Raca_Cachorro/PetApp.py
+++ Descreve_Raca_Cachorro/PetApp.py
@@ -1,42 +1,24 @@
 #criar um app para que drescreve raça de cachorro usando uma API externa
-#https://dog.ceo/dog-api/documentation/random
+#api que vamos usar: https://dog.ceo/dog-api/
 
 import requests
 import streamlit as st
 
-#criar uma função para pegar nome da raça
-def get_breed():
-    response = requests.get('https://dog.ceo/api/breeds/list/all')
-    breeds = response.json()['message']
-    return breeds
-
-#criar uma função para pegar imagem da raça
-def get_image(breed):
-    response = requests.get(f'https://dog.ceo/api/breed/{breed}/images/random')
-    image = response.json()['message']
-    return image
-
-#criar funçao para descrever a raça
-def get_description(breed):
-    response = requests.get(f'https://dog.ceo/api/breed/{breed}/list')
-    description = response.json()['message']
-    return description
-
+#função para pesquisar a raça do cachorro
 def main():
-    st.title('Dog Breed App')
-    st.sidebar.title('Dog Breed App')
-    st.markdown('This app retrieves dog breed from the [Dog API](https://dog.ceo/dog-api/)')
+    st.title('Descubra a raça do seu cachorro')
+    st.write('Digite a raça do seu cachorro')
+    raça = st.text_input('Raça do cachorro')
+    if raça:
+        #fazendo a requisição
+        response = requests.get(f'https://dog.ceo/api/breed/{raça}/images/random')
+        #pegando a resposta
+        data = response.json()
+        #pegando a url da imagem
+        url = data['message']
+        #mostrando a imagem
+        st.image(url, use_column_width=True)
 
-    breeds = get_breed()
-    breeds = list(breeds.keys())
-    breed = st.sidebar.selectbox('Breed', breeds)
-    if st.button('Show random image'):
-        image = get_image(breed)
-        st.image(image, use_column_width=True)
-
-    if st.button('Show description'):
-        description = get_description(breed)
-        st.write(description)
 
 if __name__ == '__main__':
     main()
