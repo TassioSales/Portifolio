81424740c324ac5932c8fd33c08a0177920ad394
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-05 18:12:23 -0300

    Update PetApp.py

--- Descreve_Raca_Cachorro/PetApp.py
+++ Descreve_Raca_Cachorro/PetApp.py
@@ -4,31 +4,39 @@
 import requests
 import streamlit as st
 
-#criar uma função para pegar a imagem
-def get_dog():
-    response = requests.get("https://dog.ceo/api/breeds/image/random")
-    data = response.json()
-    return data["message"]
-
-#criar uma função para pegar a raça
+#criar uma função para pegar nome da raça
 def get_breed():
-    response = requests.get("https://dog.ceo/api/breeds/list/all")
-    data = response.json()
-    return data["message"]
+    response = requests.get('https://dog.ceo/api/breeds/list/all')
+    breeds = response.json()['message']
+    return breeds
 
-#criar uma função para pegar a sub-raça
-def get_subbreed(breed):
-    response = requests.get(f"https://dog.ceo/api/breed/{breed}/list")
-    data = response.json()
-    return data["message"]
+#criar uma função para pegar imagem da raça
+def get_image(breed):
+    response = requests.get(f'https://dog.ceo/api/breed/{breed}/images/random')
+    image = response.json()['message']
+    return image
+
+#criar funçao para descrever a raça
+def get_description(breed):
+    response = requests.get(f'https://dog.ceo/api/breed/{breed}/list')
+    description = response.json()['message']
+    return description
 
 def main():
-    st.title("Dog Breed App")
-    st.image(get_dog(), width=500)
-    st.write("Select a breed to see a sub-breed")
-    breed = st.selectbox("Breed", list(get_breed().keys()))
-    subbreed = st.selectbox("Sub-breed", get_subbreed(breed))
-    st.image(get_dog(), width=500)
+    st.title('Dog Breed App')
+    st.sidebar.title('Dog Breed App')
+    st.markdown('This app retrieves dog breed from the [Dog API](https://dog.ceo/dog-api/)')
+
+    breeds = get_breed()
+    breeds = list(breeds.keys())
+    breed = st.sidebar.selectbox('Breed', breeds)
+    if st.button('Show random image'):
+        image = get_image(breed)
+        st.image(image, use_column_width=True)
+
+    if st.button('Show description'):
+        description = get_description(breed)
+        st.write(description)
 
 if __name__ == '__main__':
     main()
