263cdf9f45fac4184368dd277435e44d06de987d
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-04 23:22:07 -0300

    Update app.py

--- conselhos_online/app.py
+++ conselhos_online/app.py
@@ -3,7 +3,8 @@ import traceback
 import streamlit as st
 from py_trans import PyTranslator
 
-#função para gerar conselho aleatório
+
+# função para gerar conselho aleatório
 def get_random_advice():
     """
     Get random advice from https://api.adviceslip.com/
@@ -17,7 +18,8 @@ def get_random_advice():
     advice = response.json()["slip"]["advice"]
     return advice
 
-#função para traduzir o conselho
+
+# função para traduzir o conselho
 def translate_advice(advice, language):
     """
     Translate advice to given language
@@ -37,6 +39,7 @@ def translate_advice(advice, language):
         return advice
     return translated_advice
 
+
 def main():
     st.title("Advice Generator")
     st.markdown("## Get random advice in your language")
@@ -44,7 +47,6 @@ def main():
     advice = get_random_advice()
     translated_advice = translate_advice(advice, language)
     st.success(translated_advice)
-
 
 
 if __name__ == "__main__":
