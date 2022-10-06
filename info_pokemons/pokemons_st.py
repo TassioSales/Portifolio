5d2d7bf5a3da0ba43666f43b6ba89a30d31314d3
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-06 19:13:37 -0300

    Update pokemons_st.py

--- info_pokemons/pokemons_st.py
+++ info_pokemons/pokemons_st.py
@@ -1,16 +1,27 @@
-#criar app que recebe um pokemon e retorna suas informações
-#acessar a api  https://pokeapi.co/api/v2/pokemon/{}
+# criar app que recebe um pokemon e retorna suas informações
+# acessar a api  https://pokeapi.co/api/v2/pokemon/{}
 
 import requests
 import json
 import streamlit as st
+from lista_de_pokemons import pokemons_ordem_alfabetica
 
-def main():
-    pokemon = st.text_input("Digite o nome do pokemon: ")
+
+# criar funçaõ que retorna o pokemon que mostre o pokemon digitado
+def get_pokemon(pokemon):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
     response = requests.get(url)
     data = json.loads(response.text)
-    st.write(f"Nome: {data['name']}")
+    return data
+
+
+# criar que mostra os dados do pokemon
+def show_pokemon(data):
+    st.title(f"Nome: {data['name']}")
+    st.image(data['sprites']['front_default'])
+    st.image(data['sprites']['back_default'])
+    st.image(data['sprites']['front_shiny'])
+    st.image(data['sprites']['back_shiny'])
     st.write(f"Altura: {data['height']}")
     st.write(f"Peso: {data['weight']}")
     st.write(f"Tipo: {data['types'][0]['type']['name']}")
@@ -18,10 +29,31 @@ def main():
     st.write(f"Experiência base: {data['base_experience']}")
     st.write(f"Ordem: {data['order']}")
     st.write(f"ID: {data['id']}")
-    st.image(data['sprites']['front_default'])
-    st.image(data['sprites']['back_default'])
-    st.image(data['sprites']['front_shiny'])
-    st.image(data['sprites']['back_shiny'])
+
+
+# criar função principal
+def main():
+    # criar um título
+    st.title("Pokemons")
+    # criar um subtitulo
+    st.subheader("Digite o nome do pokemon")
+    # criar um input com o nome do pokemon preenchido com o primeiro pokemon da lista
+    pokemon = st.text_input("Digite o nome do pokemon", pokemons_ordem_alfabetica[0])
+    # criar um botão
+    if st.button("Buscar"):
+        # criar uma variável que recebe o pokemon digitado
+        data = get_pokemon(pokemon)
+        # mostrar os dados do pokemon
+        show_pokemon(data)
+    # criar um selectbox com a lista de pokemons
+    pokemon = st.selectbox("Selecione o pokemon", pokemons_ordem_alfabetica)
+    # criar um botão
+    if st.button("Buscar"):
+        # criar uma variável que recebe o pokemon digitado
+        data = get_pokemon(pokemon)
+        # mostrar os dados do pokemon
+        show_pokemon(data)
+
 
 if __name__ == "__main__":
     main()
