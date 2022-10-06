225bdf2b6783636e6f52944767ea31ff5bea0964
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-05 21:20:04 -0300

    Update PetApp.py

--- Descreve_Raca_Cachorro/PetApp.py
+++ Descreve_Raca_Cachorro/PetApp.py
@@ -27,6 +27,31 @@ def mostrar_imagem(raca):
     #mostrando a imagem
     st.image(url_imagem, use_column_width=True)
 
+#pegar descrição da raça
+def pegar_descricao(raca):
+    #fazendo a requisição
+    response = requests.get(f'https://dog.ceo/api/breed/{raca}/list')
+    #pegando o json
+    data = response.json()
+    #pegar tamanho do cachorro
+    tamanho = data['message']['height']['metric']~
+    #pegar peso do cachorro
+    peso = data['message']['weight']['metric']
+    #pegar a vida média do cachorro
+    vida = data['message']['life_span']
+    #pegar a origem do cachorro
+    origem = data['message']['origin']
+    #pegar a descrição do cachorro
+    descricao = data['message']['bred_for']
+    #mostrar as informações
+    st.subheader('Informações sobre a raça')
+    st.write(f'Tamanho: {tamanho} cm')
+    st.write(f'Peso: {peso} kg')
+    st.write(f'Vida média: {vida} anos')
+    st.write(f'Origem: {origem}')
+    st.write(f'Descrição: {descricao}')
+    
+
 
 
 #função principal
