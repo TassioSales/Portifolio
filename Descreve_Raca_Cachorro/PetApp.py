dffc04b0724ce114e318e27b2f555432f4b61a8f
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-06 13:46:46 -0300

    atualizando

--- Descreve_Raca_Cachorro/PetApp.py
+++ Descreve_Raca_Cachorro/PetApp.py
@@ -3,9 +3,6 @@
 
 import requests
 import streamlit as st
-#importa biblioteca para ler pdf
-import pdfplumber
-
 
 #função para pesquisar a raça do cachorro
 def selecionar_raca():
@@ -30,24 +27,12 @@ def mostrar_imagem(raca):
     #mostrando a imagem
     st.image(url_imagem, use_column_width=True)
 
-def ler_pdf():
-    with pdfplumber.open('C:/Users/rober/Downloads/Manual de Identificação de Raças de Cães.pdf') as pdf:
-        page = pdf.pages[0]
-        text = page.extract_text()
-        return text
-
-
-
-
-
 
 #função principal
 def main():
     st.title('Descubra a raça do seu cachorro')
     raca = selecionar_raca()
     mostrar_imagem(raca)
-    descricao = pegar_descricao(raca)
-    st.write(descricao)
 
 
 if __name__ == '__main__':
--- Descreve_Raca_Cachorro/teste.py
+++ Descreve_Raca_Cachorro/teste.py
@@ -1,10 +1,5 @@
-import pdfplumber
-
-def ler_pdf():
-    with pdfplumber.open('') as pdf:
-        page = pdf.pages[0]
-        text = page.extract_text()
-        return text
 
 
-if __name__ == '__main__':
+        
+
+
