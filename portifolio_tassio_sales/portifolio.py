acc074da7d996a09fdd1d6a92a48b7df1e39ccf8
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-06 22:37:01 -0300

    Update portifolio.py

--- portifolio_tassio_sales/portifolio.py
+++ portifolio_tassio_sales/portifolio.py
@@ -33,7 +33,7 @@ def main():
             #criar um para meu whatsapp com o icone do whatsapp e o numero do meu whatsapp e botao para abrir em outra aba
             st.markdown('<a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank"><img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" width="30px" height="30px" alt="whatsapp"></a> <a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank">WhatSapp</a>', unsafe_allow_html=True)
             #criar um link para o meu email com o icone do email e o meu email e botao para abrir em outra aba
-            st.markdown('<a href="tassiolucian.ljs@gmail.com" target="_blank"><img src="https://img.icons8.com/colored/48/000000/email.png" width="30px" height="30px" alt="email"></a> <a href="tassiolucian.ljs@gmail.com" target="_blank">Email</a>', unsafe_allow_html=True)
+            st.markdown('<a href="mailto:tassiolucian.ljs@gmail.com" target="_blank"><img src="https://img.icons8.com/colored/48/000000/email.png" width="30px" height="30px" alt="email"></a> <a href="mailto:tassiolucian.ljs@gmail.com" target="_blank">Email</a>', unsafe_allow_html=True)
             
     
 
