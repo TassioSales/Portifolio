4e8d11ee56e8daa09dc3e2b709f769b20ccc3df3
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-07 09:57:43 -0300

    Update portifolio.py

--- portifolio_tassio_sales/portifolio.py
+++ portifolio_tassio_sales/portifolio.py
@@ -6,20 +6,41 @@ import streamlit as st
 def paginacontato():
     container.markdown("<h1 style='text-align: center; color: red;'>Contato</h1>", unsafe_allow_html=True)
     container.markdown("<h2 style='text-align: center; color: red;'>Aqui voce pode me contatar</h2>", unsafe_allow_html=True)
-    col1, col2, col3 = st.columns(4)
-    with col1:
-        #criando link para conta do linkedin
-        container.markdown('<a href="https://www.linkedin.com/in/tassiosales/" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" width="30px" height="30px" alt="linkedin"></a> <a href="https://www.linkedin.com/in/tassiosales/" target="_blank">Linkedin</a>', unsafe_allow_html=True)
-    with col2:
-        #criando link para conta do github
-        container.markdown('<a href="https://github.com/TassioSales" target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" width="30px" height="30px" alt="github"></a> <a href="https://github.com/TassioSales" target="_blank">Github</a>', unsafe_allow_html=True)
-    with col3:
-        #criando link para conta do WhatSapp
-        container.markdown('<a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank"><img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" width="30px" height="30px" alt="whatsapp"></a> <a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank">WhatSapp</a>', unsafe_allow_html=True)
-    with col4:
-        #criando link para conta de email
-        container.markdown('<a href="mailto:tassiolucian.ljs@gmail.com" target="_blank"><img src="https://img.icons8.com/doodle/48/000000/newsletter.png" width="30px" height="30px" alt="email"></a> <a href="mailto:tassiolucian.ljs@gmail.com" target="_blank">Email</a>', unsafe_allow_html=True)
-    
+    #criar tabela html para colocar link de contatos
+    html = f"""
+    <table style="width:100%">
+    <tr>
+        <th>Linkedin</th>
+        <th>GitHub</th>
+        <th>Whatsaap</th>
+        <th>E-mail</th>
+        <th>FaceBook</th>
+        <th>Instagram</th>
+    </tr>
+    <tr>
+        <td>'<a href="https://www.linkedin.com/in/tassiosales/" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" width="30px" height="30px" alt="linkedin"></a> <a href="https://www.linkedin.com/in/tassiosales/" target="_blank">Linkedin</a>'Linkedin</a></td>
+        <td><a href="https://github.com/TassioSales" target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" width="30px" height="30px" alt="github"></a> <a href="https://github.com/TassioSales" target="_blank">Github</a></td>
+        <td><a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank"><img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" width="30px" height="30px" alt="whatsapp"></a> <a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank">WhatSapp</a></td>
+        <td><a href="mailto:tassiolucian.ljs@gmail.com" target="_blank"><img src="https://img.icons8.com/doodle/48/000000/newsletter.png" width="30px" height="30px" alt="email"></a> <a href="mailto:tassiolucian.ljs@gmail.com" target="_blank">Email</a>'</td>
+        <td><a href="https://www.facebook.com/tassio.sales.7" target="_blank"><img src="https://img.icons8.com/color/48/000000/facebook-new.png" width="30px" height="30px" alt="facebook"></a> <a href="https://www.facebook.com/tassio.sales.7" target="_blank">Facebook</a></td>
+        <td><a href="https://www.instagram.com/tassio_sales/" target="_blank"><img src="https://img.icons8.com/color/48/000000/instagram-new.png" width="30px" height="30px" alt="instagram"></a> <a href="https://www.instagram.com/tassio_sales/" target="_blank">Instagram</a></td>
+    </tr>
+    </table>
+    """
+    container.markdown(html, unsafe_allow_html=True)
+
+
+
+"""#criando link para conta do linkedin
+container.markdown('<a href="https://www.linkedin.com/in/tassiosales/" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" width="30px" height="30px" alt="linkedin"></a> <a href="https://www.linkedin.com/in/tassiosales/" target="_blank">Linkedin</a>', unsafe_allow_html=True)
+#criando link para conta do github
+container.markdown('<a href="https://github.com/TassioSales" target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" width="30px" height="30px" alt="github"></a> <a href="https://github.com/TassioSales" target="_blank">Github</a>', unsafe_allow_html=True)
+
+#criando link para conta do WhatSapp
+container.markdown('<a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank"><img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" width="30px" height="30px" alt="whatsapp"></a> <a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank">WhatSapp</a>', unsafe_allow_html=True)
+
+#criando link para conta de email
+container.markdown('<a href="mailto:tassiolucian.ljs@gmail.com" target="_blank"><img src="https://img.icons8.com/doodle/48/000000/newsletter.png" width="30px" height="30px" alt="email"></a> <a href="mailto:tassiolucian.ljs@gmail.com" target="_blank">Email</a>', unsafe_allow_html=True)"""
     
 
 def PaginadeMensagem():
