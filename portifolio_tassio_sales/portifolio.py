11170475034bbda26b729dfeda38f3cb9b78daa6
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-06 22:23:48 -0300

    Update portifolio.py

--- portifolio_tassio_sales/portifolio.py
+++ portifolio_tassio_sales/portifolio.py
@@ -31,8 +31,9 @@ def main():
         st.markdown('<a href="https://github.com/TassioSales" target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" width="30px" height="30px" alt="github"></a> <a href="https://github.com/TassioSales" target="_blank">Github</a>', unsafe_allow_html=True)
         #criar um para meu whatsapp com o icone do whatsapp e o numero do meu whatsapp e botao para abrir em outra aba
         st.markdown('<a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank"><img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" width="30px" height="30px" alt="whatsapp"></a> <a href="https://api.whatsapp.com/send?phone=5561982970840" target="_blank">WhatSapp</a>', unsafe_allow_html=True)
-        #criar caixa de texto com meu email 
-        st.write("Email:tassiolucian@gmail.com")
+        #criar um link para o meu email com o icone do email e o meu email e botao para abrir em outra aba
+        st.markdown('<a href="tassiolucian.ljs@gmail.com" target="_blank"><img src="#https://img.icons8.com/colored/48/000000/email.png width="30px" height="30px" alt="Email"></a> <a href="tassiolucian.ljs@gmail.com" target="_blank">E-mail</a>', unsafe_allow_html=True)
+        #criar um link para o meu email com o icone do email e o meu email e botao para abrir em outra aba
 
     
         
