53a00d3db16eaf193aeb3c41d3983d501181c627
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-07 09:33:04 -0300

    Update portifolio.py

--- portifolio_tassio_sales/portifolio.py
+++ portifolio_tassio_sales/portifolio.py
@@ -22,12 +22,6 @@ def paginacontato():
 
     
 
-
-    
-
-   
-    
-
 def PaginadeMensagem():
     #configurando h1
     st.markdown("<h1 style='text-align: center; color: red;'>Mensagem</h1>", unsafe_allow_html=True)
