1a4c863f4519b6369751d9952ca44523ca4febc5
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-10 15:04:30 -0300

    Update gerador.py

--- cria_e_valida_cpf/gerador.py
+++ cria_e_valida_cpf/gerador.py
@@ -155,8 +155,10 @@ def main():
         option = st.selectbox('Escolha uma opção', ['CPF', 'CNPJ'])
         if st.button('Gerar'):
             if option == 'CPF':
+                gerar_cpf()
                 st.success(f'O {cpf} gerado e valido')
             elif option == 'CNPJ':
+                gerar_cpf()
                 st.success(f'O {cnpj} gerado e invalido')
     else:
         st.subheader('About')
