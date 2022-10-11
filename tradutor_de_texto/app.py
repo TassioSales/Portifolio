ee5a54f4404a849e130bec2aa1c4faf0d2a0c795
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-10 23:24:41 -0300

    Update app.py

--- tradutor_de_texto/app.py
+++ tradutor_de_texto/app.py
@@ -17,20 +17,7 @@ def pedir_frase():
         st.write(e)
 
 
-# tratar texto para ser traduzido
-def tratar_texto(texto):
-    try:
-        nlp = spacy.load('pt_core_news_sm')
-        doc = nlp(texto)
-        lista = []
-        for token in doc:
-            if token.pos_ == 'PUNCT':
-                lista.append(token.text)
-            else:
-                lista.append(token.lemma_)
-        return ' '.join(lista)
-    except Exception as e:
-        st.write(e)
+
 
 
 # funçao para pedir idioma para cliente
@@ -49,7 +36,7 @@ def tradutor(texto, idioma):
     try:
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=f"Translate this into 1. {idioma}\n\n{texto} ?\n\n1.",
+            prompt=f"Translate this into 1. {idioma}\n\n{texto}\n\n1.",
             temperature=0.3,
             max_tokens=100,
             top_p=1,
@@ -63,25 +50,16 @@ def tradutor(texto, idioma):
 
 def main():
     try:
-        # Titulo
         st.title('Tradutor de Texto')
-
-        # Pedir frase para cliente
-        frase = pedir_frase()
-
-        # Tratar texto
-        texto_tratado = tratar_texto(frase)
-
-        # Pedir idioma para cliente
+        st.write('Escolha seu idioma e digite sua frase para ser traduzida')
         idioma = escolha_idioma()
-
-        # Traduzir frase
-        traducao = tradutor(texto_tratado, idioma)
-
-        # Mostrar tradução
-        st.text_area(traducao)
+        texto = pedir_frase()
+        if st.button('Traduzir'):
+            traducao = tradutor(texto, idioma)
+            st.write(traducao)
     except Exception as e:
         st.write(e)
+        
 
 
 if __name__ == '__main__':
