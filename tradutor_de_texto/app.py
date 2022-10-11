e896a5ee25f9b21d02c8a1bc3273a7fe126cedaf
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-10 21:26:08 -0300

    Update app.py

--- tradutor_de_texto/app.py
+++ tradutor_de_texto/app.py
@@ -1,3 +1,11 @@
 #criar script para tradução de texto
 
-#
+#imports 
+import traceback
+import streamlit as st
+import openai
+import spacy
+
+#funçao para pedir frase para cliente 
+def pedir_frs
+
