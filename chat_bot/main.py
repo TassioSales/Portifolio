9896d39c37c6a54b4f21aeca71d1603e1f93ba82
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-12 20:46:02 -0300

    Update main.py

--- chat_bot/main.py
+++ chat_bot/main.py
@@ -6,6 +6,11 @@ openai.api_key = st.secrets["api"]
 
 
 def chat_bot():
+    """Chat bot function
+    Returns:
+        str: Chat bot response
+    """
+
     #titulo
     st.title("Chat Bot")
     with st.form("form"):
@@ -26,6 +31,10 @@ def chat_bot():
 
 
 def chat_bot_amigos():
+    """Chat Bot com amigos
+    Returns:
+        str: Chat bot response
+    """
     #titulo,
     st.title("Chat Bot Amigos")
     with st.form("form"):
@@ -46,6 +55,11 @@ def chat_bot_amigos():
 
 
 def chat_bot_marv():
+    """Chat Bot com Marv
+    Returns: str: Chat bot response
+    """
+
+
     #titulo
     st.title("Chat Bot Marvin")
     with st.form("form"):
@@ -65,6 +79,9 @@ def chat_bot_marv():
 
 
 def chat_bot_alexa():
+    """Chat Bot com Alexa
+    Returns: str: Chat bot response
+    """
     #titulo
     st.title("Chat Bot Alexa")
     with st.form("form"):
@@ -84,29 +101,32 @@ def chat_bot_alexa():
 
 
 def chat_bot_js():
+    """Chat Bot com J.S.
+    Returns: str: Chat bot response
+    """
     #titulo
     st.title("Chato Javascript")
-    with st.form("form"):
-        question = st.text_input("Humano: ")
+    with st.form("form"): #formulario
+        question = st.text_input("Humano: ")#campo de texto
         response = openai.Completion.create(
-            model="code-davinci-002",
-            prompt=f"Humano: {question}\nJavascript:",
-            temperature=0,
-            max_tokens=60,
-            top_p=1,
-            frequency_penalty=0.5,
-            presence_penalty=0,
-            stop=["You:"]
+            model="code-davinci-002",#modelo
+            prompt=f"Humano: {question}\nJavascript:",#prompt
+            temperature=0,#temperatura
+            max_tokens=60,#maximo de tokens
+            top_p=1,#top p
+            frequency_penalty=0.5,#penalidade de frequencia
+            presence_penalty=0,#penalidade de presença
+            stop=["You:"]#parar
         )
            # enviar a mensagem para o usuário
-        st.write(f"Javascript: {response['choices'][0]['text']}")
-        st.form_submit_button("Enviar")
-        
+        st.write(f"Javascript: {response['choices'][0]['text']}")#escrever a mensagem
+        st.form_submit_button("Enviar")#botão enviar
+
 
 def main():
     st.sidebar.title("Menu")
     menu = ["Chat Bot", "Chat Bot Amigos", "Chat Bot Marvin", "Chat Bot Alexa", "Chat Bot Javascript"]
-    choice = st.sidebar.selectbox("Escolha uma opção", menu)
+    choice = st.sidebar.selectbox("Escolha uma opção", menu
     if choice == "Chat Bot":
         chat_bot()
     elif choice == "Chat Bot Amigos":
