bbfeb58910d8fd8f8945bd29f56d716c72abe316
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-11 23:44:46 -0300

    Update main.py

--- chat_bot/main.py
+++ chat_bot/main.py
@@ -6,28 +6,27 @@ openai.api_key = st.secrets["api"]
 
 
 def chat_bot():
-        with st.form("form"):
-            question = input("Humano: ")
-            response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=f"Human: {question}\nIA:",
-                temperature=0.9,
-                max_tokens=150,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0.6,
-                stop=["\n", " Human:", " IA:"]
-            )
-            #enviar a mensagem para o usuário
-            st.write(f"IA: {response['choices'][0]['text']}")
-            st.form_submit_button("Enviar")
-
+    with st.form("form"):
+        question = st.write("Humano: ")
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"Human: {question}\nIA:",
+            temperature=0.9,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6,
+            stop=["\n", " Human:", " IA:"]
+        )
+        # enviar a mensagem para o usuário
+        st.write(f"IA: {response['choices'][0]['text']}")
+        st.form_submit_button("Enviar")
 
 
 def main():
     st.title("Chatbot")
     chat_bot()
 
+
 if __name__ == "__main__":
     main()
-
