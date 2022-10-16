2d4fd3f5eda93555bee5696dfec08dace8971926
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-16 16:57:27 -0300

    Update app.py

--- analisar_tweets/app.py
+++ analisar_tweets/app.py
@@ -14,5 +14,52 @@ from textblob import TextBlob
 from collections import Counter
 import Streamlit as st
 
+#chaves de acesso
+consumer_key = st.secrets["consumer_key"]
+consumer_secret = st.secrets["consumer_secret"]
+access_token = st.secrets["access_token"]
+access_token_secret = st.secrets["access_token_secret"]
+openai.api_key = st.secrets["api"]
+
+# função para autenticar twitter
+def autenticar():
+    """[summary]
+    Função para autenticar o twitter
+    Returns:
+        [type]: [description]
+    :param: auth: autenticação do twitter
+    """
+    global api
+    auth = tw.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tw.API(auth, wait_on_rate_limit=True)
+    return api
+
+# função para perguntar ao usuário o tema que deseja
+def perguntar():
+    """[summary]
+    Função para perguntar ao usuário o tema que deseja
+    Returns:
+        [type]: string
+    :param: tema: tema que desejado
+    """
+    tema = st.text_input("Digite o tema que deseja pesquisar")
+    return tema
+
+# função pegunta ao usuário o número de tweets que deseja
+def perguntar_numero():
+    """[summary]
+    Função para perguntar ao usuário o número de tweets que deseja
+    Returns:
+        [type]: string
+    :param: numero: número de tweets que desejada
+    """
+    numero = st.text_input("Digite o número de tweets que deseja")
+    return numero
+
+
+
+
+
 
 
