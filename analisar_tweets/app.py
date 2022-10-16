c75883ef2b346c2341161ec2ed13b8884e94fa40
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-16 16:37:56 -0300

    atualizações

---
+++ analisar_tweets/app.py
@@ -0,0 +1,18 @@
+#Bibliotecas
+import os
+import openai
+import tweepy as tw
+import re
+import string
+import nltk
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from textblob import TextBlob
+from collections import Counter
+import Streamlit as st
+
+
+
