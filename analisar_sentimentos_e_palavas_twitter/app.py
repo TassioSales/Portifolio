041ca615e141f3f155128b3a73fa7853ad9649e3
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-17 00:37:50 -0300

    Update app.py

--- analisar_sentimentos_e_palavas_twitter/app.py
+++ analisar_sentimentos_e_palavas_twitter/app.py
@@ -142,15 +142,15 @@ def analisar_sentimentos_nltk(df):
             ss = sid.polarity_scores(traducao.text)
             #mostrar o sentimento dos tweets
         for k in sorted(ss):
-            #motrar o sentimento dos tweets sao positivos, negativos ou neutros
-            if k in ss:
-                if ss[k] >= 0.05:
-                    #adicionar o sentimento dos tweets ao dataframe
-                    df_sentimeto = df_sentimeto.append({'Tweets': tweet, 'Sentimento': 'Positivo'}, ignore_index=True)
-                elif ss[k] <= -0.05:
-                    df_sentimeto = df_sentimeto.append({'Tweets': tweet, 'Sentimento': 'Negativos'}, ignore_index=True)
-                else:
-                    df_sentimeto = df_sentimeto.append({'Tweets': tweet, 'Sentimento': 'Neutro'}, ignore_index=True)
+            for tweet in df['Tweets']:
+                if k in ss:
+                    if ss[k] >= 0.05:
+                        #adicionar o sentimento dos tweets ao dataframe
+                        df_sentimeto = df_sentimeto.append({'Tweets': tweet, 'Sentimento': 'Positivo'}, ignore_index=True)
+                    elif ss[k] <= -0.05:
+                        df_sentimeto = df_sentimeto.append({'Tweets': tweet, 'Sentimento': 'Negativos'}, ignore_index=True)
+                    else:
+                        df_sentimeto = df_sentimeto.append({'Tweets': tweet, 'Sentimento': 'Neutro'}, ignore_index=True)
         #mostrar o dataframe com os tweets e o sentimento
         if st.button("Mostrar Sentimento"):
             st.table(df_sentimeto)
