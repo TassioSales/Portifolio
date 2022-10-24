8850b593a325469ab5b6828f326f8423c8ce2d39
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-24 16:52:12 -0300

    updates

---
+++ tradutor_audio_e_texto/tradutor_audio_texto.py
@@ -0,0 +1,10 @@
+#baixar bibliotecas para tradução de texto
+from googletrans import Translator
+#baixar biblioteca para reconhecimento de voz
+import speech_recognition as sr
+#baixar biblioteca gtts para transformar texto em voz
+from gtts import gTTS
+#baixar biblioteca playsound para tocar o audio
+from playsound import playsound
+#baixar biblioteca Streamlit para criar a interface
+import streamlit as st
