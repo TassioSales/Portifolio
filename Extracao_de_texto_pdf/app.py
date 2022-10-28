7ecfa1dc8012b94dcc6d57217c18b614dbf73e3c
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-28 18:09:34 -0300

    updates

---
+++ Extracao_de_texto_pdf/app.py
@@ -0,0 +1,245 @@
+#criar programa bara analisar o arquivo pdf ou txt fornecido pelo usuario
+# e retornar o resultado da analise
+# importar bibliotecas
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import pdfplumber
+from stop_word import stop_palavras
+import re
+import time
+import os
+from nltk.stem import RSLPStemmer
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from googletrans import Translator
+from wordcloud import WordCloud
+import string
+import heapq
+import nltk
+import openai
+
+
+
+#função para pedir o arquivo ao usuario
+def get_file():
+    uploaded_file = st.file_uploader("Choose a file")
+    if st.button('Upload'):
+        if uploaded_file is not None:
+            with open("arquivo.pdf", "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            #mostra barra de processo de carregamento
+            my_bar = st.progress(0)
+            for percent_complete in range(100):
+                time.sleep(0.1)
+                my_bar.progress(percent_complete + 1)
+            st.success('Upload realizado com sucesso!')
+            
+def read_file_pdf():
+    #ler o arquivo pdf
+    with pdfplumber.open("arquivo.pdf") as pdf:
+        #mostrar o texto do arquivo
+        texto = ""
+        for page in pdf.pages:
+            texto += page.extract_text()
+        return texto
+   
+#tratar o texto usando o regex
+def limpar_texto(texto):
+    texto = texto.lower()
+    #remover pontuação
+    texto = re.sub(r'[^\w\s]', '', texto)
+    #remover numeros
+    texto = re.sub(r'[0-9]', '', texto)
+    #remover espaços em branco
+    texto = texto.strip()
+    #remover caracteres especiais
+    texto = re.sub(r'[^\w\s]', '', texto)
+    #remover acentos
+    texto = re.sub(r'[áàâã]', 'a', texto)
+    texto = re.sub(r'[éèê]', 'e', texto)
+    texto = re.sub(r'[íì]', 'i', texto)
+    texto = re.sub(r'[óòôõ]', 'o', texto)
+    texto = re.sub(r'[úù]', 'u', texto)
+    texto = re.sub(r'[ç]', 'c', texto)
+    return texto
+        
+def remover_stop_words(texto):
+    #remover as stop words
+    texto = texto.lower()
+    texto = re.sub(r'[^\w\s]', '', texto)
+    texto = texto.split()
+    texto = [palavra for palavra in texto if palavra not in stop_palavras]
+    texto = " ".join(texto)
+    return texto
+
+def retorna_texto():
+    if os.path.exists("arquivo.pdf"):
+        texto = read_file_pdf()
+        texto = limpar_texto(texto)
+        texto = remover_stop_words(texto)
+        return texto
+    
+#mostar dataframe com as palavras e a quantidade de vezes que aparecem
+def mostra_df():
+    dados = retorna_texto()
+    dados = dados.split()
+    dados = pd.DataFrame(dados, columns=["Palavras"])
+    dados = dados["Palavras"].value_counts()
+    dados = pd.DataFrame(dados)
+    dados = dados.rename(columns={"Palavras": "Quantidade"})
+    dados = dados.reset_index()
+    dados = dados.rename(columns={"index": "Palavras"})
+    return dados
+
+
+#mostrar o grafico de barras
+def mostra_grafico_barras(dataframe):
+    #criar grafico de barras somente com top 15 palavras
+    dados = dataframe.head(15)
+    fig = px.bar(dados, x="Palavras", y="Quantidade", color="Quantidade", title="Top 5 palavras")
+    st.plotly_chart(fig)
+    
+#mostrar o grafico de pizza
+def mostra_grafico_pizza(dataframe):
+    #criar grafico de pizza somente com top 15 palavras
+    dados = dataframe.head(5)
+    fig = px.pie(dados, values="Quantidade", names="Palavras", title="Top 15 palavras")
+    st.plotly_chart(fig)
+    
+
+    
+#criar wordcloud com as palavras 
+def mostra_grafico_nuvem():
+    texto = retorna_texto()
+    #remover palavras duplicadas
+    texto = set(texto.split())
+    texto = " ".join(texto)
+    #remover palavras com menos de 3 letras
+    texto = re.sub(r'\b\w{1,3}\b', '', texto)
+    #remover palavras com mais de 15 letras
+    texto = re.sub(r'\b\w{15,}\b', '', texto)
+    wordcloud = WordCloud(width=800, height=500, max_font_size=110).generate(texto)
+    st.image(wordcloud.to_array())
+    
+    
+#analise de sentimento da pagina escolhida pelo usuario
+def analise_sentimento():
+    arquivo = retorna_texto()
+    #pergunta ao usuario qual pagina ele quer analisar com valor padrao 1 minimo 1
+    pagina = st.number_input("Qual página você quer analisar?", min_value=1, value=1)
+    #mostrar o texto da pagina escolhida
+    if os.path.exists("arquivo.pdf"):
+        with pdfplumber.open("arquivo.pdf") as pdf:
+            texto = pdf.pages[pagina].extract_text()
+            st.write(texto)
+            #se o texo estiver em portugues, traduzir para ingles
+            if texto.isascii():
+                texto = texto
+            else:
+                translator = Translator()
+                texto = translator.translate(texto, dest="en").text
+            #analise de sentimento
+            sid = SentimentIntensityAnalyzer()
+            ss = sid.polarity_scores(texto)
+            st.write(ss)
+            if ss["compound"] >= 0.05:
+                st.success("O Sentimento dessa pagina e positivo")
+            elif ss["compound"] <= -0.05:
+                st.write("O Sentimento dessa pagina Negativo")
+            else:
+                st.write("O Sentimento dessa pagina Neutro")
+                
+        
+        
+#criar função pra criar resumo do usando openai
+def resumo_texto():
+    #abrir chave da api
+    openai.api_key = st.secrets['api']
+    texto = retorna_texto()
+    response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=f"{texto}.\n\nTl;dr",
+    temperature=0.7,
+    max_tokens=60,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+    )
+    st.write(response["choices"][0]["text"])
+    
+    
+    
+    
+    
+    
+    
+
+
+
+     
+                
+          
+def main():
+    #criar menu
+    menu = ["Upload", "Mostrar Texto original", "Mostrar Texto tratado", "Mostrar DataFrame", "Mostrar Gráfico Barras", "Mostrar Gráfico Pizza", "Analise de Sentimento", "wordcloud", "Resumo"]
+    choice = st.sidebar.selectbox("Menu", menu)
+    if choice == "Upload":
+        st.title("Upload de arquivo")
+        st.subheader("Por favor faça upload do arquivo PDF")
+        get_file()
+    elif choice == "Mostrar Texto original":
+        st.title("Trecho do Texto original")
+        #procurar o arquivo usando arquivo.pdf ou arquivo.txt
+        if os.path.exists("arquivo.pdf"):
+            arquivo = "arquivo.pdf"
+            #mostra a pagina 10 usando pdfplumber
+            with pdfplumber.open(arquivo) as pdf:
+                page = pdf.pages[10]
+                st.write(page.extract_text())
+    elif choice == "Mostrar Texto tratado":
+        st.markdown("<h1 style='text-align: center; color: white;'>Trecho Tratado</h1>", unsafe_allow_html=True)
+        texto = retorna_texto()
+        st.write(texto)
+        
+    elif choice == "Mostrar DataFrame":
+        st.markdown("<h1 style='text-align: center; color: white;'>Tabela</h1>", unsafe_allow_html=True)
+        dataframe = mostra_df()
+        #mostrar dataframe alinhado ao centro
+        st.table(dataframe.style.set_properties(**{'text-align': 'center'}))
+        
+    elif choice == "Mostrar Gráfico Barras":
+        st.markdown("<h1 style='text-align: center; color: white;'>Grafico Barra</h1>", unsafe_allow_html=True)
+        dataframe = mostra_df()
+        mostra_grafico_barras(dataframe)
+        
+    elif choice == "Mostrar Gráfico Pizza":
+        st.markdown("<h1 style='text-align: center; color: white;'>Grafico Pizza</h1>", unsafe_allow_html=True)
+        dataframe = mostra_df()
+        mostra_grafico_pizza(dataframe)
+        
+    elif choice == "Analise de Sentimento":
+        #criar um h1 no streamlit com o titulo Analise de Sentimento alinha ao centro
+        st.markdown("<h1 style='text-align: center; color: white;'>Analise de Sentimento</h1>", unsafe_allow_html=True)
+        analise_sentimento()
+        
+    elif choice == "wordcloud":
+        st.markdown("<h1 style='text-align: center; color: white;'>Nuvem de palavras</h1>", unsafe_allow_html=True)
+        dataframe = mostra_df()
+        mostra_grafico_nuvem()
+        
+    elif choice == "Resumo":
+        st.markdown("<h1 style='text-align: center; color: white;'>Resumo</h1>", unsafe_allow_html=True)
+        #criar botao para gerar o resumo
+        if st.button("Gerar Resumo", key="resumo", help="Clique aqui para gerar o resumo"):
+            resumo_texto()
+
+        
+                        
+
+    
+        
+if __name__ == '__main__':
+    main()
--- tradutor_e_classificador_de_texto/app.py
+++ tradutor_e_classificador_de_texto/app.py
@@ -1,8 +1,8 @@
-#biblioteca para traduzir o texto
+# biblioteca para traduzir o texto
 from googletrans import Translator
-#biblioteca para ambiente web
+# biblioteca para ambiente web
 import streamlit as st
-#usar nltk para classificar o texto
+# usar nltk para classificar o texto
 import nltk
 from nltk.stem import RSLPStemmer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -10,18 +10,25 @@ import re
 from bases import stop_palavras
 from nltk.tokenize import word_tokenize
 from nrclex import NRCLex
+import plotly.express as px
+import pandas as pd
 import json
+
 nltk.download('all')
-#função para pedir texto para o usuário
+
+
+# função para pedir texto para o usuário
 def get_text():
     text = st.text_area("Digite o texto a ser traduzido")
     if text:
         return text
 
-#menu de idiomas para tradução
+
+# menu de idiomas para tradução
 def get_lang():
     try:
-        lang = st.selectbox("Selecione o idioma",("Alemão","Inglês","Espanhol","Francês","Italiano","Japonês","Coreano","Chinês"))
+        lang = st.selectbox("Selecione o idioma",
+                            ("Alemão", "Inglês", "Espanhol", "Francês", "Italiano", "Japonês", "Coreano", "Chinês"))
         if lang == "Alemão":
             return "de"
         elif lang == "Inglês":
@@ -39,30 +46,34 @@ def get_lang():
         elif lang == "Chinês":
             return "zh-CN"
     except Exception as e:
-        print("Erro ao selecionar idioma",e)
+        print("Erro ao selecionar idioma", e)
 
-#traduzir o texto do português para o idioma selecionado
+
+# traduzir o texto do português para o idioma selecionado
 def translate_text(text, lang):
     translator = Translator()
     try:
         translated = translator.translate(text, dest=lang)
         return translated.text
     except Exception as e:
-        print("Erro ao traduzir o texto",e)
-        
-#remover stopwords do texto
+        print("Erro ao traduzir o texto", e)
+
+
+# remover stopwords do texto
 def remove_stopwords(text):
     stopwords = stop_palavras
     text = [word for word in text if word not in stopwords]
     return text
-        
-#função para traduzir o texto sempre para o inglês
+
+
+# função para traduzir o texto sempre para o inglês
 def traduzirParaIngles(texto):
     translator = Translator()
     texto = translator.translate(texto, dest="en")
     return texto.text
 
-#funçao para stemizar o texto
+
+# funçao para stemizar o texto
 def Stemize(text):
     stemmer = RSLPStemmer()
     stem = []
@@ -70,27 +81,30 @@ def Stemize(text):
         stem.append(stemmer.stem(i))
     return stem
 
-#função para tokenizar o texto
+
+# função para tokenizar o texto
 def Tokenize(text):
     text = word_tokenize(text)
     return text
 
+
 def tratar_texto(text):
-    #traduzir o texto para o inglês
+    # traduzir o texto para o inglês
     text = traduzirParaIngles(text)
-    #tokenizar o texto
+    # tokenizar o texto
     text = Tokenize(text)
-    #remover stopwords do texto
+    # remover stopwords do texto
     text = remove_stopwords(text)
-    #stemizar o texto
+    # stemizar o texto
     text = Stemize(text)
     return text
 
-#função para analisar sentimento do texto
+
+# função para analisar sentimento do texto
 def analisar_sentimento(text):
     sid = SentimentIntensityAnalyzer()
     score = sid.polarity_scores(text)
-    #retornar se o texto é positivo, negativo ou neutro
+    # retornar se o texto é positivo, negativo ou neutro
     st.write(score)
     if score['compound'] > 0.0:
         st.text("O sentimento do texto é positivo")
@@ -98,13 +112,11 @@ def analisar_sentimento(text):
         st.write("O sentimento do texto é negativo")
     else:
         st.write("O sentimento do texto é neutro")
-        
-#função para analisar emoções do texto
-def analisar_emocoes(text):
+
+
+def CriarDataFrameEmocoes(text):
     emotion = NRCLex(text)
-    #mostra emoçoes do texto em forma de dicionário
-    st.write(emotion.raw_emotion_scores)
-    #pegar resultado do de cada emoção
+    # pegar resultado do de cada emoção
     anger = emotion.raw_emotion_scores['anger']
     anticipation = emotion.raw_emotion_scores['anticipation']
     disgust = emotion.raw_emotion_scores['disgust']
@@ -113,42 +125,88 @@ def analisar_emocoes(text):
     sadness = emotion.raw_emotion_scores['sadness']
     surprise = emotion.raw_emotion_scores['surprise']
     trust = emotion.raw_emotion_scores['trust']
+    # criar um dicionário com as emoções e seus valores
+    emocoes = {"Raiva": anger, "Antecipação": anticipation, "Nojo": disgust, "Medo": fear, "Alegria": joy,
+               "Tristeza": sadness, "Surpresa": surprise, "Confiança": trust}
+    # criar um dataframe com as emoções e seus valores
+    df = pd.DataFrame(emocoes.items(), columns=['Emoção', 'Valor'])
+    return df
+
+
+def CriarDataFrameSentimentos(text):
+    emotion = NRCLex(text)
+    # pegar resultado do de cada emoção
     positivo = emotion.raw_emotion_scores['positive']
     negativo = emotion.raw_emotion_scores['negative']
-    #criar um dicionário com as emoções e seus valores
-    emocoes = {"Raiva":anger,"Antecipação":anticipation,"Nojo":disgust,"Medo":fear,"Alegria":joy,"Tristeza":sadness,"Surpresa":surprise,"Confiança":trust}
-    sentimento = {"Positivo":positivo, "Negativo":negativo}
-    #organizar o dicionário em ordem decrescente
-    emocoes = sorted(emocoes.items(), key=lambda x: x[1], reverse=True)
-    #mostrar o top 3 de emoções
-    st.write(f'A {emocoes[0][0]} é a emoção mais forte do texto sua pontuação é {emocoes[0][1]}')
-    st.write(f'A {emocoes[1][0]} é a segunda emoção mais forte do texto sua pontuação é {emocoes[1][1]}')
-    st.write(f'A {emocoes[2][0]} é a terceira emoção mais forte do texto sua potuação é {emocoes[2][1]}')
-    #mostrar o top se o texto é positivo ou negativo
-    if sentimento[0][1] > sentmento[1][1]:
-        st.write("Em resumo o texto é positivo")
-    else:
-        st.write("Em resumo o texto é negativo")
-            
-        
+    # criar um dicionário com as emoções e seus valores
+    sentimento = {"Positivo": positivo, "Negativo": negativo}
+    # criar um dataframe com as emoções e seus valores
+    df = pd.DataFrame(sentimento.items(), columns=['Sentimento', 'Valor'])
+    return df
+
+
+# função para analisar emoções do texto
+def analisar_emocoes(df):
+    st.markdown(df.to_html(index=False), unsafe_allow_html=True)
+    # mostra top 3 emoções
+    st.write("Top 3 emoções")
+    st.write(df.sort_values(by=['Valor'], ascending=False).head(3))
+    # mostra gráfico de barras
+    fig = px.bar(df, x='Emoção', y='Valor', title='Emoções', color='Valor')
+    st.plotly_chart(fig)
+
+
+def analisar_sentimentos(df):
+    st.markdown(df.to_html(index=False), unsafe_allow_html=True)
+    # mostra gráfico de barras
+    fig = px.bar(df, x='Sentimento', y='Valor', title='Sentimentos', color='Valor')
+    st.plotly_chart(fig)
+
+
 def main():
-    st.title("Tradutor de Texto")
-    text = get_text()
-    lang = get_lang()
-    #criar um botão para traduzir o texto
-    if st.button("Traduzir"):
-        translated_text = translate_text(text, lang)
-        st.text_area(f"Tradução para {lang}",translated_text)
-    #criar um botão para classificar o texto
-    if st.button("Classificar"):    
-        text = tratar_texto(text)
-        text = ' '.join(text)
-        analisar_sentimento(text)
-    if st.button("Analisar emoções"):
-        text = tratar_texto(text)
-        text = ' '.join(text)
-        analisar_emocoes(text)
-        
+    # criar interface com menuS
+    menu = ["Home", "Analisar Texto", "Analise de Sentimento", "Analise de Emoções",
+            "Analise de Sentimentos Secundários", "Tradutor"]
+    choice = st.sidebar.selectbox("Menu", menu)
+    if choice == "Home":
+        st.subheader("Home")
+        st.text("Bem vindo ao projeto de análise de sentimentos")
+        st.text("Escolha uma opção no menu")
+    elif choice == "Analisar Texto":
+        st.subheader("Analisar Texto")
+        text = st.text_area("Digite o texto")
+        if st.button("Analisar"):
+            st.write(text)
+    elif choice == "Analise de Sentimento":
+        st.subheader("Analise de Sentimento")
+        text = st.text_area("Digite o texto")
+        if st.button("Analisar"):
+            text = tratar_texto(text)
+            text = ' '.join(text)
+            analisar_sentimento(text)
+    elif choice == "Analise de Emoções":
+        st.subheader("Analise de Emoções")
+        text = st.text_area("Digite o texto")
+        if st.button("Analisar"):
+            text = tratar_texto(text)
+            text = ' '.join(text)
+            df = CriarDataFrameEmocoes(text)
+            analisar_emocoes(df)
+    elif choice == "Analise de Sentimentos Secundários":
+        st.subheader("Analise de Sentimentos Secundários")
+        text = st.text_area("Digite o texto")
+        if st.button("Analisar"):
+            text = tratar_texto(text)
+            text = ' '.join(text)
+            df = CriarDataFrameSentimentos(text)
+            analisar_sentimentos(df)
+    elif choice == "Tradutor":
+        text = get_text()
+        lang = get_lang()
+        if st.button("Traduzir"):
+            traducao = translate_text(text, lang)
+            st.write(traducao)
+
+
 if __name__ == "__main__":
     main()
-    
