2df675e8273c914ce7d8fbb2a91a538b5858958a
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-31 17:09:44 -0300

    Update app.py

--- Extracao_de_texto_pdf/app.py
+++ Extracao_de_texto_pdf/app.py
@@ -227,7 +227,7 @@ def main():
         st.markdown("<h1 style='text-align: center; color: white;'>Resumo</h1>", unsafe_allow_html=True)
         #criar botao para gerar o resumo
         if st.button("Gerar Resumo", key="resumo", help="Clique aqui para gerar o resumo"):
-            resumo_texto()
+            sumarize_text_portugues()
 
          
 if __name__ == '__main__':
