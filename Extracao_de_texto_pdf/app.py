d2fc0f197238d24455eda5d7db933353fcf1090b
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-31 19:18:34 -0300

    Update app.py

--- Extracao_de_texto_pdf/app.py
+++ Extracao_de_texto_pdf/app.py
@@ -247,12 +247,11 @@ def main():
             sumarize_text_portugues(n_send)
             pdf = FPDF()
             pdf.add_page()
-            pdf.set_font("Arial", size=15)
-            f = open("resumo.txt", "r")
-            for x in f:
-                pdf.cell(200, 10, txt=x, ln=1, align='C')
+            pdf.set_font("Arial", size=12)
+            pdf.cell(200, 10, txt="Resumo", ln=1, align="C")
+            pdf.cell(200, 10, txt="Resumo", ln=1, align="C")
             pdf.output("resumo.pdf")
-            st.success("Arquivo gerado com sucesso")    
+            st.success("Arquivo gerado com sucesso")
 
                 
 
