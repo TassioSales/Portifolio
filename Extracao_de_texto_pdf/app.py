c1387f23e87ad380c728b20ad92c77ed0d6ccb3d
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-10-31 15:50:47 -0300

    Update app.py

--- Extracao_de_texto_pdf/app.py
+++ Extracao_de_texto_pdf/app.py
@@ -160,18 +160,19 @@ def resumo_texto():
     #abrir chave da api
     openai.api_key = st.secrets['api']
     texto = retorna_texto()
-    #criar resumo do texto
+    #chamar openai para criar resumo
     response = openai.Completion.create(
-      engine="davinci",
-      prompt=texto,
-      temperature=0.9,
-      max_tokens=150,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0.6
+        engine="davinci",
+        prompt=texto,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=["\n"]
     )
+    #mostrar resumo
     st.write(response["choices"][0]["text"])
-    
 
 def main():
     #criar menu
