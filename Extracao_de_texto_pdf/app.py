f383e029c1ef71cef25c517ec7973051475cef16
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-11-03 12:49:08 -0300

    Update app.py

--- Extracao_de_texto_pdf/app.py
+++ Extracao_de_texto_pdf/app.py
@@ -227,37 +227,37 @@ def resumo_texto_pagina(pagina):
 
 def resumo_geral(text, per):
     try:
-        def summarize(text, per):
-            nlp = spacy.load("pt_core_news_sm")
-            doc = nlp(text)
-            tokens = [token.text for token in doc]
-            word_frequencies = {}
-            for word in doc:
-                if word.text.lower() not in list(STOP_WORDS):
-                    if word.text.lower() not in punctuation:
-                        if word.text not in word_frequencies.keys():
-                            word_frequencies[word.text] = 1
-                        else:
-                            word_frequencies[word.text] += 1
-            max_frequency = max(word_frequencies.values())
-            for word in word_frequencies.keys():
-                word_frequencies[word] = word_frequencies[word] / max_frequency
-            sentence_tokens = [sent for sent in doc.sents]
-            sentence_scores = {}
-            for sent in sentence_tokens:
-                for word in sent:
-                    if word.text.lower() in word_frequencies.keys():
-                        if sent not in sentence_scores.keys():
-                            sentence_scores[sent] = word_frequencies[word.text.lower()]
-                        else:
-                            sentence_scores[sent] += word_frequencies[word.text.lower()]
-            select_length = int(len(sentence_tokens) * per)
-            summary = nlargest(select_length, sentence_scores, key=sentence_scores.get)
-            final_summary = [word.text for word in summary]
-            summary = ''.join(final_summary)
-            return summary
-
-
+        nlp = spacy.load("pt_core_news_sm")
+        doc = nlp(text)
+        tokens = [token.text for token in doc]
+        word_frequencies = {}
+        for word in doc:
+            if word.text.lower() not in list(STOP_WORDS):
+                if word.text.lower() not in punctuation:
+                    if word.text not in word_frequencies.keys():
+                        word_frequencies[word.text] = 1
+                    else:
+                        word_frequencies[word.text] += 1
+        max_frequency = max(word_frequencies.values())
+        for word in word_frequencies.keys():
+            word_frequencies[word] = word_frequencies[word] / max_frequency
+        sentence_tokens = [sent for sent in doc.sents]
+        sentence_scores = {}
+        for sent in sentence_tokens:
+            for word in sent:
+                if word.text.lower() in word_frequencies.keys():
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word.text.lower()]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word.text.lower()]
+        select_length = int(len(sentence_tokens) * per)
+        summary = nlargest(select_length, sentence_scores, key=sentence_scores.get)
+        final_summary = [word.text for word in summary]
+        summary = ''.join(final_summary)
+        return summary
+    except Exception as e:
+        st.error(e)
+        st.warning("Erro ao gerar o resumo")
 def main():
     # criar menu
     menu = ["Upload", "Mostrar Texto original", "Mostrar Texto tratado", "Mostrar DataFrame", "Mostrar Gráfico Barras",
